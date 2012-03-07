5f8da326bb2a0dff9a3e7bb37722ac986868ef91
Colin Thomas-Arnold <colinta@mac.com>
2012-03-07 16:15:20 -0700

    long overdue refactor, and added 'clipboard_manager_show' command.

--- clipboard_manager.py
+++ clipboard_manager.py
@@ -14,11 +14,31 @@ class HistoryList(list):
     def at(self, idx):
         self.__index = idx if idx < len(self) else 0
 
+    def show(self):
+        ret = ""
+        ret += " CLIPBOARD HISTORY (%d)\n" % len(self)
+        ret += "====================%s==\n" % ("=" * len(str(len(self))))
+        for i, item in enumerate(self):
+            if i == self.__index:
+                ret += '--> '
+            else:
+                ret += '    '
+            item = item.replace("\t", "\\t")
+            item = item.replace("\n", "\\n")
+            item = item.replace("\r", "\\r")
+            ret += str(i + 1) + '. ' + item + "\n"
+        return ret
+
     def append(self, item):
-        self.insert(0, item)
-        self.__index = 0
-        if len(self) > self.SIZE:
-            del self[self.SIZE:]
+        """
+        Appends to the history only if it isn't the current
+        item.
+        """
+        if not self or self[self.__index] != item:
+            self.insert(0, item)
+            self.__index = 0
+            if len(self) > self.SIZE:
+                del self[self.SIZE:]
 
     def current(self):
         if len(self) == 0:
@@ -28,95 +48,97 @@ class HistoryList(list):
     def next(self):
         if self.__index > 0:
             self.__index -= 1
+        self.status()
 
     def previous(self):
         if self.__index < len(self) - 1:
             self.__index += 1
+        self.status()
 
     def first(self):
         """"first" actually kind of means "last", since this is a FIFO stack"""
         self.__index = len(self) - 1
+        self.status()
 
     def last(self):
         """"last" actually kind of means "first", since this is a FIFO stack"""
         self.__index = 0
+        self.status()
+
+    def status(self):
+        sublime.status_message('Set Clipboard to "' + self.current() + '"')
+        sublime.set_clipboard(self.current())
 
 
-HISTORY = HistoryList()
+HISTORY = HistoryList([sublime.get_clipboard()])
 
 
-class ClipboardManagerBase(sublime_plugin.TextCommand):
-    def update_clipboard(self, content):
-        sublime.status_message('Set Clipboard to "' + content + '"')
-        sublime.set_clipboard(content)
-
-    def next(self):
-        HISTORY.next()
-        self.update_clipboard(HISTORY.current())
-
-    def previous(self):
-        HISTORY.previous()
-        self.update_clipboard(HISTORY.current())
-
-    def appendClipboard(self):
-        # append the contents of the clipboard to the history
-        if not HISTORY or HISTORY[0] != sublime.get_clipboard():
-            HISTORY.append(sublime.get_clipboard())
+def append_clipboard():
+    # append the contents of the clipboard to the history
+    HISTORY.append(sublime.get_clipboard())
 
 
-class ClipboardManagerPaste(ClipboardManagerBase):
+class ClipboardManagerPaste(sublime_plugin.TextCommand):
     def run(self, edit, indent=False):
-        # If the user pastes something that was copied in a different program, it will not be in sublime's buffer, so we attempt to append every time
-        self.appendClipboard()
         if indent:
             self.view.run_command('paste_and_indent')
         else:
             self.view.run_command('paste')
 
 
-class ClipboardManagerCut(ClipboardManagerBase):
+class ClipboardManagerCut(sublime_plugin.TextCommand):
     def run(self, edit):
         # First run sublime's command to extract the selected text.
         # This will set the cut/copy'd data on the clipboard which we can easily steal without recreating the cut/copy logic.
         self.view.run_command('cut')
-        self.appendClipboard()
+        append_clipboard()
 
 
-class ClipboardManagerCopy(ClipboardManagerBase):
+class ClipboardManagerCopy(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.run_command('copy')
-        self.appendClipboard()
+        append_clipboard()
 
 
-class ClipboardManagerNext(ClipboardManagerBase):
+class ClipboardManagerNext(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.next()
+        HISTORY.next()
 
 
-class ClipboardManagerNextAndPaste(ClipboardManagerBase):
+class ClipboardManagerNextAndPaste(sublime_plugin.TextCommand):
     def run(self, edit, indent=False):
-        self.next()
+        HISTORY.next()
         if indent:
             self.view.run_command('paste_and_indent')
         else:
             self.view.run_command('paste')
 
 
-class ClipboardManagerPrevious(ClipboardManagerBase):
+class ClipboardManagerPrevious(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.previous()
+        HISTORY.previous()
 
 
-class ClipboardManagerPreviousAndPaste(ClipboardManagerBase):
+class ClipboardManagerPreviousAndPaste(sublime_plugin.TextCommand):
     def run(self, edit, indent=False):
-        self.previous()
+        HISTORY.previous()
         if indent:
             self.view.run_command('paste_and_indent')
         else:
             self.view.run_command('paste')
 
 
-class ClipboardManagerChooseAndPaste(ClipboardManagerBase):
+class ClipboardManagerShow(sublime_plugin.WindowCommand):
+    def run(self):
+        v = self.window.get_output_panel('clipboard_manager')
+        e = v.begin_edit('clipboard_manager')
+        v.replace(e, sublime.Region(0, v.size()), '')
+        v.insert(e, 0, HISTORY.show())
+        v.end_edit(e)
+        self.window.run_command('show_panel', {'panel': 'output.clipboard_manager'})
+
+
+class ClipboardManagerChooseAndPaste(sublime_plugin.TextCommand):
     def run(self, edit):
         def format(line):
             return line.replace('\n', '\\n')[:64]
@@ -133,10 +155,14 @@ class ClipboardManagerChooseAndPaste(ClipboardManagerBase):
             if idx >= 0:
                 idx = line_map[idx]
                 HISTORY.at(idx)
-                self.update_clipboard(HISTORY.current())
                 self.view.run_command('paste')
 
         if lines:
             sublime.active_window().show_quick_panel(lines, on_done)
         else:
             sublime.status_message('Nothing in history')
+
+
+class ClipboardManagerEventListener(sublime_plugin.EventListener):
+    def on_activated(self, view):
+        append_clipboard()
